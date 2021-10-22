f57d0aa49827eb9e5c2b97a4fa43c6beb0d6f319
Eric-Joshua Oke <yeshua7dm@gmail.com>
2021-10-22 11:48:00 +0100

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 // use hooks for state in functional component
 // replaces the need for class components
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Run a football game",
+      day: "Feb 5th at 2pm",
+      reminder: true,
+    }, 
+    {
+      id: 2,
+      text: "Borrowed a football game",
+      day: "April 5th at 2pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "steal a football game",
+      day: "Jun 5th at 2pm",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "Buy a football",
+      day: "May 5th at 2pm",
+      reminder: true,
+    },
+  ]);
 
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Run a football game',
-            day: 'Feb 5th at 2pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Borrowed a football game',
-            day: 'April 5th at 2pm',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'steal a football game',
-            day: 'Jun 5th at 2pm',
-            reminder: true
-        },
-        {
-            id: 4,
-            text: 'Buy a football',
-            day: 'May 5th at 2pm',
-            reminder: true
-        }
-    ]
-)
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task)=> task.id !== id))
+  }
 
   return (
-    <div className="App">
-        <Header/>
-        <Tasks tasks={tasks}/>
+    <div className="container">
+        <Header />
+        <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({task}) => {
+const Task = ({task, onDelete}) => {
     return (
         <div className='task'>
-          <h3>{task.text} <FaTimes/> </h3>  
+          <h3>{task.text} 
+          <FaTimes className='color-red' onClick={() => onDelete(task.id)}/> </h3>  
           <p>{task.day}</p>
         </div>
     )
