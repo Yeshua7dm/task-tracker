b933cb033993bdd44155b815b22dad78bb2fbb3f
Eric-Joshua Oke <yeshua7dm@gmail.com>
2021-10-23 00:02:44 +0100

    added json server, updated delete and add functions

--- src/App.js
+++ src/App.js
@@ -8,30 +8,47 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [showForm, setShowForm] = useState(false)
-  const [tasks, setTasks] = useState([
-  ]);
+  const [tasks, setTasks] = useState([]);
 
-  // useEffect(() => {
-  //   const getTasks = async() => {
-  //     const response = await fetch('https://localhost:5000/tasks')
-  //     const data = await response.json()
-  //     console.log(data)
-  //   }
+  useEffect(() => {
+    const getTasks = async() => {
+      const TasksFromServer = await fetchTasks()
+      setTasks(TasksFromServer)
+    }
 
-  //   getTasks()
-  // }, [])
+    getTasks()
+  }, [])
 
-  // ceate the function to delete task from the UI
-  const deleteTask = (id) => {
+  // fetch the tasks
+  const fetchTasks = async() => {
+    const response = await fetch('http://localhost:5000/tasks')
+    const data = await response.json()
+    return data
+  }
+
+  // the function to delete task from the UI
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE"
+    })
+
     setTasks(tasks.filter((task)=> task.id !== id))
   }
 // create function to add task to the displayed tasks
-  const addTask = (task) => {
-    console.log(task)
+  const addTask = async(task) => {
+    const resp = await fetch('http://localhost:5000/tasks', {
+      method: "POST",
+      headers : {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await resp.json()
+    setTasks([...tasks, data])
     
-    const id = tasks.length === 0 ? 1 : tasks[tasks.length-1].id + 1;
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+    // const id = tasks.length === 0 ? 1 : tasks[tasks.length-1].id + 1;
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
   }
 
 // function to toggle reminder
